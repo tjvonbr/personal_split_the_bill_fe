c8a154a82f56baacae628aad5c5473936bb31649
eralpkor <eralp.ny@gmail.com>
2019-09-25 12:03:23 -0400

    added table

--- src/components/TableNav.js
+++ src/components/TableNav.js
@@ -1,39 +1,39 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import styled from "styled-components";
-// import TableImage from '../img/table_with_view.jpg';
+import styled from "styled-components";
+import TableImage from '../img/table_with_view.jpg';
 
 
-// const Header = styled.header`
-//   width: 100%;
-//   background: #56baed;
-//   height: 60px;
-// `
-// const Nav = styled.nav`
-//   display: flex;
-//   align-content: center;
-//   align-items: center;
-//   height: 100%;
-//   padding: 0 1rem;
-// `
+const Header = styled.header`
+  width: 100%;
+  background: #56baed;
+  height: 60px;
+`
+const Nav = styled.nav`
+  display: flex;
+  align-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 0 1rem;
+`
 
-// const Image = styled.img`
-//   object-fit: cover;
-//   width: 100%;
-//   height: 300px;
-//   margin-bottom: -4px;
+const Image = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 300px;
+  margin-bottom: -4px;
   
-// `
+`
 
 
-// const TableNav = () => {
-//   return (
-//     <>
-//     <Image src={TableImage} alt='table' />
-   
-//     </>
-//   );
-// };
+const TableNav = () => {
+  return (
+    <>
+    <Image src={TableImage} alt='table' />
+  
+    </>
+  );
+};
 
-// export default TableNav;
+export default TableNav;
